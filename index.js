4864257eb2fbf4ede87c65f4c6ec81167ac208b8
Israel <Israel.pro1994@gmail.com>
2020-07-22 15:46:53 -0500

    I am planning out how to proceed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 console.log('hello world!')
 
-const pods = document.createElement("div")
+const smallPod = document.createElement("div")
 
+
+const mediumPod = document.createElement("div")
+
+
+const largePod = document.createElement("div")
 
 
 const playButton = document.createElement('button')
+playButton.innerText = 'Press To Play'
 
+const timer = document.getElementById('timer')
+timer.innerText = '4:30'
