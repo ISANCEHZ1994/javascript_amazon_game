16a361cba3e9e77ca3c84ff0f7694b56603a596d
Israel <Israel.pro1994@gmail.com>
2020-07-23 18:49:00 -0500

    creating li for the rules

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log('hello world!')
 
 const smallPod = document.createElement("div")
 const mediumPod = document.createElement("div")
@@ -11,18 +10,24 @@ playButton.innerText = 'Press To Play'
 // introContainer.append(playButton) why does append NOT work in this case?
 
 
+const rulesContainer = document.querySelector('#rules')
+const li = document.createElement('li')
+li.setAttribute('id','one-rule')
 
-const rules = document.getElementById('rules')
-// const rulesLi = document.createElement('li')
 const rulesArry = [ 'Items MUST be organized HEAVIEST at the bottom and LIGHTEST at the top', 
                     'A row MUST NOT be over the MAX WEIGHT', 'Some items can only be place at a specifc pod', 
                     'You CANNOT place the same items PARALLEL or PERPENDICULAR to each other (think either a Checkers or Halo shape)',
-                    'You must have AT LEAST 8 containers of items at your station', 'When placing an item in a ']
+                    'You must have AT LEAST 8 containers of items at your station']
 
-                    
+const rules = rulesArry.map( function(rule){
+    console.log(rule)
+return `<li> ${rule} <li>`
+})
 
+// li.append(rules)
+rulesContainer.append(rules)
 
-
+// THIS IS THE COUNTDOWN TIMER <----------------------------------------------           
 // https://www.youtube.com/watch?v=LAaf7-WuJJQ
 let countDown;
 
@@ -47,10 +52,17 @@ function timer(seconds){
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds / 60); // minutes should be whole numbers 
     const remainderSeconds = seconds % 60 ; // we get the remainder that is actually the seconds
-    const displayTime = `${minutes}:${remainderSeconds < 10 ? '0' : ''}`
+    const displayTime = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}` // otherwise the countdown will look like this 1:1 instead of 1:01
     const timer = document.getElementById('timer')
     timer.innerText = displayTime 
     console.log(displayTime)
+
+    if(displayTime == '0:00'){
+        const timesUp = document.getElementById('times-up')
+        timesUp.innerText = 'TIMES UP'
+    }
 };
 
-timer(124)
+
+
+timer(900) // 900 seconds is 15 minutes 
