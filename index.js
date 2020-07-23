b0801d494c34eeb2edccebb9dd528ae1f94cce16
Israel <Israel.pro1994@gmail.com>
2020-07-23 15:56:35 -0500

    working on countdown timer

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-console.log('hello world!')
+// console.log('hello world!')
 
 const smallPod = document.createElement("div")
-
-
 const mediumPod = document.createElement("div")
-
-
 const largePod = document.createElement("div")
+
 
 const introContainer = document.getElementsByClassName('intro-container')
 const playButton = document.createElement('button')
 playButton.innerText = 'Press To Play'
-// introContainer.append(playButton)
+// introContainer.append(playButton) why does append NOT work in this case?
 
 
 
@@ -34,30 +31,24 @@ const rulesArry = [ 'Items MUST be organized HEAVIEST at the bottom and LIGHTEST
 
 // setInterval(setTimer,1000)
 
+// https://www.youtube.com/watch?v=LAaf7-WuJJQ
+let countDown;
 
+function timer(seconds){
+    // console.log(seconds)
+    const now = Date.now();
+    const then = now + seconds * 1000; // 1000 represents miliseconds which is means 1 second
+    console.log({now,then})
 
-// Using this site as a reference to make a countdown/timer https://codepen.io/ishanbakshi/pen/pgzNMv 
+    countDown = setInterval( () => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000); // otherwise it would have been by miliseconds
+        console.log(secondsLeft)
+        if(secondsLeft < 0){
+            clearInterval(countDown);
+            return; 
+        }
+    }, 1000) // this means that it will run every second
+}
 
-// document.getElementById('timer').innerHTML =
-//   005 + ":" + 00;
-// startTimer();
+timer(10)
 
-// function startTimer() {
-//   var presentTime = document.getElementById('timer').innerHTML;
-//   var timeArray = presentTime.split(/[:]+/);
-//   var m = timeArray[0];
-//   var s = checkSecond((timeArray[1] - 1));
-//   if(s==59){m=m-1}
-//   //if(m<0){alert('timer completed')}
-  
-//   document.getElementById('timer').innerHTML =
-//     m + ":" + s;
-//   console.log(m)
-//   setTimeout(startTimer, 1000);
-// }
-
-// function checkSecond(sec) {
-//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-//   if (sec < 0) {sec = "59"};
-//   return sec;
-// }
